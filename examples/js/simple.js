09386afc9fb010cedd984533a631097118aefc85
yukihirop <te108186@gmail.com>
2022-03-21 16:04:40 +0900

    feat: 🎸 add js mongoose sample

---
+++ examples/js/simple.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const TaskSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+const insertTasks = async () => {
+  await Promise.all(
+    [...Array(10)].map((_, index) => {
+      return Task.create([
+        {
+          title: `title_${index}`,
+          content: `content_${index}`,
+        },
+      ])
+    })
+  )
+};
+
+mongoose
+  .connect("mongodb://localhost:27017/js")
+  .catch((err) => console.err(err));
+const db = mongoose.connection;
+
+Promise.resolve(() => db.once("open"))
+  .then(() => console.log("connection opened"))
+  .then(() => insertTasks())
+  .then(() => db.close())
+  .then(() => console.log('connection closed'));
