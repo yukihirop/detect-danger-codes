93cc96423a2513dc8babe70c24937ae8fdd6e989
yukihirop <te108186@gmail.com>
2022-04-18 02:29:09 +0900

    feat: 🎸 add examples

--- packages/core/examples/ts/files/simple.ts
+++ packages/core/examples/ts/files/simple.ts
@@ -6,16 +6,40 @@ interface TaskField {
   content: string
 }
 
-type TaskDocument = mongoose.Document<mongoose.Types.ObjectId> & TaskField
+interface UserField {
+  _id: string,
+  name: string
+}
 
-interface TaskModel extends mongoose.Model<TaskDocument> {}
+type TaskDocument = mongoose.Document<mongoose.Types.ObjectId> & TaskField
+type UserDocument = mongoose.Document<mongoose.Types.ObjectId> & UserField
+
+interface TaskModel extends mongoose.Model<TaskDocument> { }
+interface UserModel extends mongoose.Model<UserDocument> { }
 
 const TaskSchema = new mongoose.Schema<TaskDocument, TaskModel>({
   title: String,
   content: String,
 });
+const UserSchema = new mongoose.Schema<UserDocument, UserModel>({
+  name: String
+})
 
 const Task = mongoose.model("Task", TaskSchema);
+const User = mongoose.model("User", UserSchema)
+
+const insertUsers = async () => {
+  await Promise.all(
+    // @ts-expect-error
+    [...Array(10)].map<any[]>((_, index) => {
+      return User.create([
+        {
+          name: `user_${index}` as string
+        }
+      ])
+    })
+  )
+};
 
 // comment
 const insertTasks = async () => {
@@ -39,6 +63,7 @@ const db = mongoose.connection;
 
 Promise.resolve(() => db.once("open", () => {}))
   .then(() => console.log("connection opened"))
+  .then(() => insertUsers())
   .then(() => insertTasks())
   .then(() => db.close())
   .then(() => console.log("connection closed"));
